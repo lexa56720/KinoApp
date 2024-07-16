53a6c7ee31eb940344edcca2d254f3a274715049
lexa56720 <lexa.0606x@gmail.com>
2024-07-16 17:06:07 +0700

    Requester added

---
+++ KinoApiWrapper/Api/Requester.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KinoApiWrapper.Api
+{
+    internal class Requester : IDisposable
+    {
+        private readonly HttpClient client;
+        public Requester(string apiKey)
+        {
+            client = new HttpClient()
+            {
+                BaseAddress = new Uri("https://kinopoiskapiunofficial.tech"),
+            };
+            client.DefaultRequestHeaders.Add("X-API-KEY", apiKey);
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public void Dispose()
+        {
+            ((IDisposable)client).Dispose();
+        }
+
+        public async Task<string> Request(string apiUrl)
+        {
+            return await client.GetStringAsync(apiUrl);
+        }
+    }
+}
